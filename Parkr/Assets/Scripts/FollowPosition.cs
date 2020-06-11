0ef6a2eb59e1a8257337dc6eb2e57474859f348e
Robert Deppe <contact@robertdeppe.de>
2020-06-11 16:42:46 +0200

    feat: script for following the position of a GO
    This is wip on #5 to fix the _sensors touching the ground_ problem.

---
+++ Parkr/Assets/Scripts/FollowPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPosition : MonoBehaviour
+{
+    public GameObject objectToFollow;
+    Transform followTransform;
+    Vector3 relationToFollowObject;
+
+    void Start()
+    {
+        followTransform = objectToFollow.GetComponent<Transform>();
+        relationToFollowObject = followTransform.position - transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = followTransform.position - relationToFollowObject;
+    }
+}
