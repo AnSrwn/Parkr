f794d350a277407cfbe5dcdda56a907a4df5a388
Robert Deppe <contact@robertdeppe.de>
2020-07-09 18:11:30 +0200

    feat: show completed episodes and reward in HUD

---
+++ Parkr/Assets/dataVisualizer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dataVisualizer : MonoBehaviour
+{
+    public CarAgent agent;
+    public GameObject cumulativeRewardText;
+    public GameObject episodesCompletedText;
+
+    Text cumulativeReward;
+    Text episodesCompleted;
+
+    private void Start()
+    {
+        cumulativeReward = cumulativeRewardText.GetComponent<Text>();
+        episodesCompleted = episodesCompletedText.GetComponent<Text>();
+    }
+    void Update()
+    {
+        cumulativeReward.text = "Cumulative Reward: " + agent.GetCumulativeReward().ToString();
+        episodesCompleted.text = "Episodes Completed: " + agent.CompletedEpisodes.ToString();
+    }
+}
